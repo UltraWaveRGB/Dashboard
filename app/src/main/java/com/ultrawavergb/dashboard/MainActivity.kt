d1b2db404455672a3d387b43b1c0eac5c095ab2a
REIS0 <aor@cesar.school>
2021-10-09 13:22:01 -0300

    Remake interface, first draft

--- app/src/main/java/com/ultrawavergb/dashboard/MainActivity.kt
+++ app/src/main/java/com/ultrawavergb/dashboard/MainActivity.kt
@@ -1,92 +1,111 @@
 package com.ultrawavergb.dashboard
 
-import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.SeekBar
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
     private val database = Firebase.database
-    private var ledValue: Int = 2
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // teste da conexao
         val myRef = database.getReference("message")
         myRef.setValue("There's a maggot!")
 
-        val btnLed = findViewById<Button>(R.id.led_button)
+        val btnPipoca = findViewById<Button>(R.id.btn_pipoca)
+        val btnLasanha = findViewById<Button>(R.id.btn_lasanha)
+        val btnBrigadeiro = findViewById<Button>(R.id.btn_brigadeiro)
+        val btnVegetais = findViewById<Button>(R.id.btn_vegetais)
+        val btnArroz = findViewById<Button>(R.id.btn_arroz)
+        val btnCarne = findViewById<Button>(R.id.btn_carne)
 
-        getLedData()
-        updateLdr()
+        val btnStart = findViewById<Button>(R.id.btn_start)
+        val btnStop = findViewById<Button>(R.id.btn_stop_cancel)
 
-        btnLed.setOnClickListener {
+        val seekBar = findViewById<SeekBar>(R.id.skbar_potencia)
+        val txtPotencia =
+            findViewById<TextView>(R.id.txtview_potencia)  // ? separar em dois text view ?
 
-            val ledRef = database.getReference("led")
+        /* --- FUNCOES DOS WIDGETS --- */
 
-            if (ledValue == 1) {
-                ledRef.setValue(0)
-                Log.d("STATE", "Led set to 0.")
-            } else if (ledValue == 0) {
-                ledRef.setValue(1)
-                Log.d("STATE", "Led set to 1.")
-            }
-
-            getLedData()
-        }
-    }
-
-    private fun getLedData() {
-        val ledRef = database.getReference("led")
-        ledRef.get().addOnSuccessListener {
-            ledValue = it.value.toString().toInt()
-            Log.d("CHANGE", "Set ledValue to ${ledValue}.")
-            Log.d("STATE", "Value: ${it.value}")
-            updateBtnColor()
-        }.addOnFailureListener {
-            Log.e("ERROR", "Error getting data", it)
-        }
-    }
-
-    private fun updateBtnColor() {
-        val btnLed = findViewById<Button>(R.id.led_button)
-
-        if (ledValue == 0) {
-            btnLed.setBackgroundColor(Color.rgb(0, 0, 0))
-            btnLed.setTextColor(Color.rgb(255, 255, 255))
-            Log.d("CHANGE", "Set button to dark.")
-        } else if (ledValue == 1) {
-            btnLed.setBackgroundColor(Color.rgb(255, 255, 255))
-            btnLed.setTextColor(Color.rgb(0, 0, 0))
-            Log.d("CHANGE", "Set button to light.")
+        btnPipoca.setOnClickListener {
+            // TODO
         }
 
-    }
+        btnLasanha.setOnClickListener {
+            // TODO
+        }
 
-    private fun updateLdr() {
-        val refLdr = database.getReference("light")
-        val textViewLdrValue = findViewById<TextView>(R.id.text_view_ldr_value)
-        refLdr.addValueEventListener(object: ValueEventListener {
+        btnBrigadeiro.setOnClickListener {
+            // TODO
+        }
 
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val value = snapshot.getValue<Int>()
-                Log.d("STATE", "LDR Value is: " + value)
-                textViewLdrValue.text = value.toString()
+        btnVegetais.setOnClickListener {
+            // TODO
+        }
+
+        btnArroz.setOnClickListener {
+            // TODO
+        }
+
+        btnCarne.setOnClickListener {
+            // TODO
+        }
+
+        btnStart.setOnClickListener {
+            val startRef = database.getReference("start")
+            startRef.setValue(1)
+            Log.d("STATE", "Start set to 1.")
+        }
+
+        btnStop.setOnClickListener {
+            // TODO
+        }
+
+
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, value: Int, p2: Boolean) {
+                txtPotencia.setText("Potencia: $value")
             }
 
-            override fun onCancelled(error: DatabaseError) {
-                Log.w("ERRO", "Failed to read value.", error.toException())
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
             }
+
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
+            }
+
         })
+
+
     }
+
+
+//    // TODO: timer
+//    private fun updateTime() {
+//        val refLdr = database.getReference("time")
+//        val textViewTimeValue = findViewById<TextView>(R.id.text_view_time_value)
+//        refLdr.addValueEventListener(object : ValueEventListener {
+//
+//            override fun onDataChange(snapshot: DataSnapshot) {
+//                val value = snapshot.getValue<Int>()
+//                Log.d("STATE", "Time Value is: " + value)
+//                textViewTimeValue.text = value.toString()
+//            }
+//
+//            override fun onCancelled(error: DatabaseError) {
+//                Log.w("ERRO", "Failed to read value.", error.toException())
+//            }
+//        })
+//    }
 
 }
